dfc220b08f342d523a703a62ac89789f2ddc4a62
lunleo <lunleo@users.noreply.github.com>
2020-10-26 23:13:22 +0200

    1
    1

--- scripts.js
+++ scripts.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const db = require('./queries')
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(bodyParser.json())
 app.use(
@@ -11,14 +11,54 @@ app.use(
   })
 )
 
-app.get('/', (request, response) => {
-  response.json({ info: 'Node.js, Express, and Postgres API' })
-})
-
 app.get('/users', db.getUsers)
 app.get('/users/:id', db.getUserById)
-
+app.use(express.static("resume"));
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
 })
+
+
+
+
+
+/*
+const http = require('http')
+const fs = require('fs')
+const path = require('path')
+
+const server = http.createServer((req, res) => {
+	let filePath = path.join(__dirname, 'resume', req.url === '/' ? 'index.html': req.url)
+
+	const ext = path.extname(filePath)
+	if (!ext) filePath += '.html'
+
+	let contentType = 'text/html'
+	if (ext === '.css') contentType = 'text/css'
+	if (ext === '.js') contentType = 'text/javascript'
+
+	fs.readFile(filePath, (err, content) => {
+		if (err){
+			fs.readFile(path.join(__dirname, 'resume', 'error.html'), (err, data) => {
+				if (err) {
+					res.writeHead(500)
+					res.end('Error')
+
+				} else {
+					res.writeHead(200, {'Content-Type': 'text/html'})
+					res.end(data)
+				}
+			})
+		} else{
+			res.writeHead(200, {'Content-Type': contentType})
+			res.end(content)
+		}
+	})
+})
+
+const PORT = process.env.PORT || 3000
+server.listen(PORT, () => {
+	console.log('Server has been started on', PORT)
+})
+*/
